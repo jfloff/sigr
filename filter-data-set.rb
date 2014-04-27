3b240903da6e51383174df9c35af24beef643759
jfloff <jfloff@gmail.com>
2014-04-27 22:26:33 +0100

    filter by city done

--- filter-data-set.rb
+++ filter-data-set.rb
@@ -29,41 +29,44 @@ city_box = {
   }
 }
 
-file_list = 'file_list.txt'
+file_list = ARGV[0]
 # base_path = './'
 base_path = 'http://dmir.inesc-id.pt/shinra/~bmartins/twitter-data/data/'
 
-File.open('geo_tweets.gz', 'a') do |write_file|
-  write_gz = Zlib::GzipWriter.new(write_file)
+File.open(file_list).read.each_line do |filename|
+  puts "Parsing #{filename.chomp} ..."
+  File.open("geo_#{filename[0...-4]}.gz", 'w') do |write_file|
+    write_gz = Zlib::GzipWriter.new(write_file)
 
-  File.open(file_list).read.each_line do |filename|
-    puts "Parsing #{filename.chomp} ..."
     file = open(base_path + filename.chomp)
     gz = Zlib::GzipReader.new(file, {encoding: Encoding::UTF_8})
-    gz.each_line do |tweet_json|
-      tweet = JSON.parse(tweet_json)
-      if !tweet['coordinates'].nil?
-        msg = tweet['text']
-        coordinates = {
-          lat: tweet['coordinates']['coordinates'][1],
-          lon: tweet['coordinates']['coordinates'][0]
-        }
 
-        simple_tweet = JSON.generate({
-          text: msg,
-          coordinates: coordinates,
-        });
+    begin
+      gz.each_line do |tweet_json|
+        tweet = JSON.parse(tweet_json)
+        if !tweet['coordinates'].nil?
+          msg = tweet['text']
+          coordinates = {
+            lat: tweet['coordinates']['coordinates'][1],
+            lon: tweet['coordinates']['coordinates'][0]
+          }
 
-        write_gz.write simple_tweet
-        # if within_box?(city_box, tweet_coordinates)
-        #   puts JSON.pretty_generate(tweet)
-        # end
+          simple_tweet = JSON.generate({
+            text: msg,
+            coordinates: coordinates,
+          });
+
+          write_gz.write(simple_tweet + "\n")
+          # if within_box?(city_box, tweet_coordinates)
+          #   puts JSON.pretty_generate(tweet)
+          # end
+        end
       end
+    ensure
+      gz.close
+      write_gz.close
     end
-    gz.close
   end
-
-  write_gz.close
   puts "Parsing #{filename.chomp} ended"
 end
 puts "The End."
